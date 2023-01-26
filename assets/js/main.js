88e2517db3695cb0749716e80854f35171d44932
RoquePersia <roque.persia149677@alumnos.info.unlp.edu.ar>
2023-01-26 18:21:04 -0300

    sexto commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,22 +97,30 @@ window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 const themeButton = document.getElementById('theme-button')
+const root = document.querySelector(":root")
 const darkTheme = 'dark-theme'
 const iconTheme = 'ri-sun-line'
+const mountain = ''
+let imgBlanca = 'url(../img/decor-section-bottom.webp)'
+let imgNegra = 'url(../img/Untitled.png)'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
+console.log(selectedTheme);
+
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+  getCurrentTheme() === 'dark' ? root.style.setProperty("--mountains", imgNegra) : root.style.setProperty("--mountains", imgBlanca)
 }
 
 // Activate / deactivate the theme manually with the button
@@ -120,9 +128,14 @@ themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
+
+ 
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
+
+    getCurrentTheme() === 'dark' ? root.style.setProperty("--mountains", imgNegra) : root.style.setProperty("--mountains", imgBlanca)
+
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
@@ -140,3 +153,6 @@ sr.reveal(`.home__social`, {delay: 600})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
+sr.reveal(`.titulo__story`,{origin: 'top'})
+sr.reveal(`.wide__title`,{origin: 'left'})
+sr.reveal(`.wide__text`, {delay: 1000})
